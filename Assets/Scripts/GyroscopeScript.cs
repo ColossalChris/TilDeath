89f2b984f4cae92307a6e8fa9b73ecb5620e634d
ChrisKlinglerOhsome <christopherklingler@Christophers-MacBook-Pro.local>
2017-07-19 20:27:48 -0600

    Initial commit

---
+++ Assets/Scripts/GyroscopeScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroscopeScript : MonoBehaviour {
+
+	public Transform player;
+	public Transform head;
+	public Transform root;
+
+	private Gyroscope gyro;
+	private bool gyroSupported;
+	private Quaternion rotationFix;
+
+	//to fix rotation this gyro needs a parent transform with this rotation: Quaternion.Euler (90, 180, 0);
+
+	// Use this for initialization
+	void Start () {
+
+		gyroSupported = SystemInfo.supportsGyroscope;
+
+		if (gyroSupported) {
+			gyro = Input.gyro;
+			gyro.enabled = true;
+
+			rotationFix = new Quaternion (0, 0, 1, 0);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void LateUpdate(){
+		if (gyroSupported) {
+			transform.localRotation = gyro.attitude * rotationFix;
+		}
+
+		root.transform.position = head.position;
+		player.rotation = Quaternion.Euler (0, transform.rotation.eulerAngles.y, 0);
+	}
+}
---
+++ Assets/Scripts/LocatorScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mapbox.Utils;
+using Mapbox.Unity.Map;
+
+public class LocatorScript : MonoBehaviour {
+
+	AbstractMap map;
+
+	[SerializeField]
+	Text locationText;
+
+	// Use this for initialization
+	IEnumerator Start()
+	{
+		// First, check if user has location service enabled
+		if (!Input.location.isEnabledByUser)
+			yield break;
+
+		// Start service before querying location
+		Input.location.Start();
+
+		// Wait until service initializes
+		int maxWait = 20;
+		while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+		{
+			yield return new WaitForSeconds(1);
+			maxWait--;
+		}
+
+		// Service didn't initialize in 20 seconds
+		if (maxWait < 1)
+		{
+			locationText.text = "Timed out";
+			yield break;
+		}
+
+		// Connection has failed
+		if (Input.location.status == LocationServiceStatus.Failed)
+		{
+			locationText.text = "Unable to determine device location";
+			yield break;
+		}
+		else
+		{
+			// Access granted and location value could be retrieved
+			locationText.text = "Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude
+				+ " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
+			map.CenterLatitudeLongitude = new Vector2d(Input.location.lastData.latitude, Input.location.lastData.longitude);
+		}
+
+		// Stop service if there is no need to query location updates continuously
+		Input.location.Stop();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
